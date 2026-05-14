5b22c9fe0061045e3739c79822d8d22828f705c2
KasiditBoonnum <kasiditboonnum@gmail.com>
2026-05-14 09:25:10 +0700

    Fix Exo start: use fixed libp2p port 5679 on all nodes

--- scripts/exo/start-exo.sh
+++ scripts/exo/start-exo.sh
@@ -4,9 +4,4 @@
 VENV="$HOME/exo-venv"
 HOSTNAME=$(hostname -s)
 
-if [ "$HOSTNAME" = "llm-01" ]; then
-    "$VENV/bin/exo" --api-port 5678 --libp2p-port 5679
-else
-    "$VENV/bin/exo" --api-port 5678 --libp2p-port 5679 \
-        --bootstrap-peers /ip4/192.168.10.11/tcp/5679
-fi
+"$VENV/bin/exo" --api-port 5678 --libp2p-port 5679
